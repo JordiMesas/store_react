51bb8573ee83f8ace1fcd71e169c634eb8eec2e4
Jordi <67053157+JordiMesas@users.noreply.github.com>
2021-02-10 13:44:57 +0100

    add removeProducts in checkout container

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -14,7 +14,7 @@ const useInitialState = () => {
 	const removeFromCart = (payload) => {
 		setState({
 			...state,
-			cart: state.cart.filter((items) => items.id !== payload.id),
+			cart: state.cart.filter((items) => {return items.id !== payload.id}),
 		});
 	};
 
